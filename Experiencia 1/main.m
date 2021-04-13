455fc47911e03bea98f691536503dba315afb1ca
TomasIbaceta <tomas.ibaceta@sansano.usm.cl>
2021-04-13 19:54:00 -0400

    randomly generated seed

--- Experiencia 1/main.m
+++ Experiencia 1/main.m
@@ -1,13 +1,14 @@
 %%experiencia 1 - actividad 1
 %y_s = y sampleado cada 0.5s
 %---------------------
-%datos para la entrada de simulink
 clc
 clear
-
+%datos para la entrada de simulink
+rand_seed = round(rand()*100)
 a1 = 1;
 a2 = 2;
 out = sim('exp1') %run the simulation
+
 %datos de salida de simulink
 y_k = out.y_k;
 u1_k = out.step;
@@ -20,7 +21,7 @@ t = linspace(0,10,length(y_k)); %base de tiempo discreta en [s]
 %Metodo de mínimos cuadrados
 %syms a_ls b_ls
 phi = [u1_k u2_k];
-Theta = pinv(phi) * y_k
-Theta2 = inv(transpose(phi)*phi) * transpose(phi) * y_k %da el mismo valor
+Theta = vpa(pinv(phi) * y_k) %para usar mas decimales %metodo usando pinv
+Theta2 = vpa(inv(transpose(phi)*phi) * transpose(phi) * y_k) %metodo manual
 %[a_ls; b_ls] = Theta  %preguntar al profe como asignar un vector columna
 %----------------
